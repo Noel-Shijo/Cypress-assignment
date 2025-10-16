2b8eb239da02c5cef3721f464409b2313de86c52
Noel-Shijo <140782953+Noel-Shijo@users.noreply.github.com>
2025-10-16 21:16:07 +0530

    Update usecase3.cy.js

--- Assessment/cypress/e2e/usecase3.cy.js
+++ Assessment/cypress/e2e/usecase3.cy.js
@@ -6,9 +6,25 @@ describe('template spec', () => {
     cy.contains("Document Automation").should('be.visible').click();
     //cy.contains("Create Learning Instance",{ timeout: 10000 }).should('be.visible').click();
     //cy.get('button[name=create-button][aria-label$="Create Learning Instance"]',{ timeout: 10000 }).click();
-    //cy.get(.).should('be.visible').click();
+    //cy.get('button[name="create-button"]',{timeout:10000}).click();
+
+    /*const now = new Date();
+    const formattedDateTime = now.toISOString().slice(0, 16); // e.g., "2025-10-16T15:30"
+
+    cy.get('input[name="name"]', { timeout: 10000 })
+    .should('be.visible')
+    .type(formattedDateTime);
+
+    cy.get('input[name="description"]', { timeout: 10000 })
+    .should('be.visible')
+    .type("testdescription");
+
+    cy.contains('button', 'Next').click();
+    cy.contains('button', 'Create').click();*/
+
   
 
   
   });
 });
+
